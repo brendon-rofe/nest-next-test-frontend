e3fe6b5c0091628f979c140228b0be760a205244
Bravo1B9 <b1@xgmi.com>
2022-11-03 05:07:57 +0000

    Initial create user and user card setup

---
+++ components/CreateUserForm.tsx
@@ -0,0 +1,72 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+export default function CreateUserForm() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Create User
+          </Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>Name</FormLabel>
+                  <Input type="text" />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="password" isRequired>
+              <FormLabel>Age</FormLabel>
+              <InputGroup>
+                <Input type={'number'} />
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Create
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import Nav from "./components/Nav"
+import Nav from "../components/Nav";
+import UserCard from "../components/UserCard";
+import CreateUserForm from "../components/CreateUserForm";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <CreateUserForm />
+      <UserCard />
     </>
   )
 }
